641ea7414ba5565e52d78d7c26fbdcc0b12e75a1
ilyaLobeuko <114007539+ilyaLobeuko@users.noreply.github.com>
2022-10-29 19:28:39 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+int main() {
+	/*setlocale(LC_ALL, "rus");
+	int x;
+	int k = 0;
+	int summa = 0;
+	do {
+		cin >> x;
+		k++;
+		summa += x;
+	} while (x != 0);
+	double sr = (double)summa / k;
+	cout << k << " " << summa << " " << sr << endl;*/
+	/*int a, b, i, summa = 0;
+	cin >> a >> b;
+	i = a;
+	do{
+		if (i % 2 != 0) {
+			summa += i;
+		}
+		i += 1;
+	} while (i <= b);
+	cout << summa;*/
+	/*int i, h;
+	cin >> h;
+	i = 1;
+	do{
+		for (int j = 1; j <= h - 1; j++)
+			cout << " ";
+		for (int j = 1; j <= 2 * i - 1; j++)
+			cout << '^';
+		cout << endl;
+		i++;
+	} while (i <= h);*/
+	/*17. В ЭВМ вводятся по очереди данные о росте N учащихся класса.Определить средний рост учащихся класса.*/
+	/*int rost, summa = 0, N, i = 1;
+	cin >> N;
+	do {
+		cin >> rost;
+		summa += rost;
+		i++;
+	} while (i <= N);
+	double sr = (double)summa / N;
+	cout << sr << endl;*/
+	//29. с помощью оператора do-while напишите программу вывода всех четных чисел в диапазоне от 2 до 100 включительно.
+	/*int i;
+	i = 2;
+	do {
+		if (i % 2 == 0) {
+			cout << i << endl;
+		}
+		i += 1;
+	} while (i <= 100);*/
+	//30. С помощью оператора do-while напишите программу определения суммы всех нечетных чисел в диапазоне от 1 до 99 включительно.
+	int 
+
+
+}
